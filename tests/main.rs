1afb4856dbe1bbe78dc08d23d9e419c65834ee10
Aster <galaster@foxmail.com>
2020-10-10 15:12:53 +0800

    🔮 Support display mode and inline mode

--- tests/main.rs
+++ tests/main.rs
@@ -1,17 +1,15 @@
+use katex_wasmbind::KaTeXOptions;
+use wasm_bindgen::JsValue;
 use wasm_bindgen_test::*;
-use syntect_yew::{render_to_string, KaTeXOptions};
 
-
-#[wasm_bindgen_test]
-fn test() {
-    let d = KaTeXOptions::display_mode();
-    let i = KaTeXOptions::inline_mode();
-
-    assert_eq!(d.render("\\frac12"), i.render("\\frac12"));
+#[test]
+fn print() {
+    println!("{:?}", JsValue::from_serde(&KaTeXOptions::default()).unwrap())
 }
 
-
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1, 1);
+fn mode() {
+    let d = KaTeXOptions::display_mode();
+    let i = KaTeXOptions::inline_mode();
+    assert_ne!(d.render("\\frac12"), i.render("\\frac12"));
 }
